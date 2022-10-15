9f6e295027468e578856477b174cbf47130517b5
Timotej Kovacka <2478424k@student.gla.ac.uk>
2022-10-15 16:43:50 +0100

    Research papers pipeline added

--- notes/lookup.py
+++ notes/lookup.py
@@ -1,9 +1,30 @@
 
+import fileinput
 import re
 
 RESOURCES = "./notes/resources.bib"
+LINKS = "./notes/resources_links.txt"
+RESEARCH_PAPERS = "./notes/research_papers.md"
 
+titles, links = [], []
 with open(RESOURCES) as f:
     for line in f:
-        title = re.search('.*?\{(.*)}.*', line)
-        print(title)
+        match = re.search('title={(.*?)}', line)
+        if match is None:
+            continue
+        titles += [match.group()[7:-1]]
+
+with open(LINKS) as f:
+    links = [line for line in f]
+
+with open(RESEARCH_PAPERS, 'r') as f:
+    filedata = f.read()
+
+filedata = filedata.split('\n')[:8]
+
+for title, link in zip(titles, links):
+    filedata += ["[{}]({})".format(title, link)]
+
+with open(RESEARCH_PAPERS, 'w') as f:
+    for line in filedata:
+        print(line, file=f)
